afc5830910d60bc5be02a19e3879104b4e6c9e48
Josef Fruehwald <JoFrhwld@gmail.com>
2014-07-29 15:39:10 +0100

    added my_svg device

--- knit_post.R
+++ knit_post.R
@@ -1,4 +1,11 @@
-knit_post <- function(input, outdir = NULL, base.url = "/") {
+my_svg <- function(file, width, height) {
+  library(RSvgDevice)
+  devSVG(file = file, width = width, height = height, bg = "transparent", fg = "black",
+         onefile = TRUE, xmlHeader = TRUE)
+}
+
+
+knit_post <- function(input, outdir = NULL, base.url = "/", fig.dir = NULL) {
   require(knitr)
   if(is.null(outdir)){
     output <- sub(".Rmd$",".md",input)
@@ -6,7 +13,11 @@ knit_post <- function(input, outdir = NULL, base.url = "/") {
     output <- file.path(outdir, sub(".Rmd$", ".md", basename(input)))
   }
   opts_knit$set(base.url = base.url)
-  fig.path <- file.path(outdir, "figs", sub(".Rmd$", "", basename(input)))
+  if(is.null(fig.dir)){
+    fig.path <- file.path(outdir, "figs", sub(".Rmd$", "", basename(input)))
+  }else{
+    fig.path <- file.path(fig.dir, sub(".Rmd$", "", basename(input))) 
+  }
   opts_chunk$set(fig.path = fig.path)
   opts_chunk$set(fig.cap = "center")
   render_jekyll(highlight="prettify")

d2790c97bf3387e736a76110d481b4a30335f22d
Josef Fruehwald <JoFrhwld@gmail.com>
2015-06-12 16:17:14 +0100

    might have figures hacked

--- _posts/2015-2-26-test_figs.md
+++ _posts/2015-2-26-test_figs.md
@@ -2,7 +2,7 @@
 layout: post
 title: "Testing Figures"
 comments: true
-date: 2015-02-26 12:44:00
+date: 2015-06-12 16:16:59
 author: [{name: "Joe", url: "jofrhwld.github.io"}]
 ---
   
@@ -11,8 +11,25 @@ I'm not sure if I'm doing figures right.
 
 
 <pre><code class="prettyprint ">  library(ggplot2)
-  library(dplyr)
-  library(magrittr)
+  library(dplyr)</code></pre>
+
+
+
+<pre><code>## 
+## Attaching package: 'dplyr'
+## 
+## The following object is masked from 'package:stats':
+## 
+##     filter
+## 
+## The following objects are masked from 'package:base':
+## 
+##     intersect, setdiff, setequal, union
+</code></pre>
+
+
+
+<pre><code class="prettyprint ">  library(magrittr)
 
   faithful %&lt;&gt;%
     mutate(clust = kmeans(., centers = 2) %&gt;% extract2(&quot;cluster&quot;) %&gt;% factor())
@@ -21,4 +38,4 @@ I'm not sure if I'm doing figures right.
   ggplot(faithful, aes(waiting, eruptions, color = clust))+
     geom_point()</code></pre>
 
-![center]({{site.baseurl}}/figs/2015-2-26-test_figsunnamed-chunk-1.svg) 
+![center]({{site.baseurl}}/figs/2015-2-26-test_figsunnamed-chunk-1-1.svg) 
--- knit_post.R
+++ knit_post.R
@@ -4,6 +4,18 @@ my_svg <- function(file, width, height) {
          onefile = TRUE, xmlHeader = TRUE)
 }
 
+inject_jekyll <- function(mdfile){
+  inConn <- file(mdfile)
+  lines <- readLines(inConn)
+  close(inConn)
+
+  for(idx in seq(along=lines)){
+    lines[idx] <- gsub("(!\\[.+\\]\\()(/figs)", "\\1{{site.baseurl}}\\2", lines[idx])
+  }
+  outConn <- file(mdfile)
+  writeLines(lines, outConn)
+  close(outConn)
+}
 
 knit_post <- function(input, outdir = NULL, base.url = "/", fig.dir = NULL) {
   require(knitr)
@@ -14,7 +26,7 @@ knit_post <- function(input, outdir = NULL, base.url = "/", fig.dir = NULL) {
   }
   opts_knit$set(base.url = base.url)
   if(is.null(fig.dir)){
-    fig.path <- file.path(outdir, "figs", sub(".Rmd$", "", basename(input)))
+    fig.path <- file.path("/figs", sub(".Rmd$", "", basename(input)))
   }else{
     fig.path <- file.path(fig.dir, sub(".Rmd$", "", basename(input))) 
   }
@@ -22,4 +34,7 @@ knit_post <- function(input, outdir = NULL, base.url = "/", fig.dir = NULL) {
   opts_chunk$set(fig.cap = "center")
   render_jekyll(highlight="prettify")
   knit(input, output=output, envir = parent.frame())
+  inject_jekyll(output)
 }
+
+
